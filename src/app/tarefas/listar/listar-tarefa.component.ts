10cfd164093b37293c3d66b5c75761ba0bf0bd56
GabrielCiriaco <gabrielcfornitano@gmail.com>
2021-09-24 09:45:57 -0300

    projeto 3 finalizado

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TarefaService} from '../shared/tarefa.service';
 import { Tarefa } from '../shared/tarefa.model';
+import { TarefaConcluidaDirective } from '../shared/tarefa-concluida.directive';
 
 @Component({
   selector: 'app-listar-tarefa',
@@ -19,5 +20,21 @@ export class ListarTarefaComponent implements OnInit {
     return this.tarefaService.listarTodos();
   }
 
+  remover($event: any, tarefa:Tarefa):void{
+    $event.preventDefault();
+    if(confirm('Deseja remover a tarefa"' + tarefa.nome + '"?')){
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.tarefaService.listarTodos();
+    }
+  }
 
+  alterarStatus(tarefa: Tarefa): void{
+    if(confirm('Deseja alterar o status da tarefa' + tarefa.nome + '"?"')){
+      this.tarefaService.alterarStatus(tarefa.id);
+      
+    }
+    this.tarefas = this.listarTodos();
+  }
+
+  
 }
